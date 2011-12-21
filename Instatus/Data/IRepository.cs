0fd867397f6e26932ab106d11161d30a85038ea6
Andy Booth <andy.booth.uk@googlemail.com>
2011-12-21 11:51:05 +0000

    IRepository fix, reference type constraint

--- Instatus/Data/IRepository.cs
+++ Instatus/Data/IRepository.cs
@@ -6,7 +6,7 @@ using System.Data.Entity;
 
 namespace Instatus.Data
 {
-    public interface IRepository<T> : IDisposable
+    public interface IRepository<T> : IDisposable where T : class
     {
         IDbSet<T> Items { get; }
         int SaveChanges();
