6bf756cafa4e4fd3654dbe897fe2083f6399248c
Andy Booth <andy.booth.uk@googlemail.com>
2011-12-23 12:12:10 +0000

    Geo and Cache extensions

---
+++ Instatus/Extensions/CacheExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+
+namespace Instatus
+{
+    public static class CacheExtensions
+    {
+        public static T Value<T>(this Cache cache, Func<T> getter, string key = null, int duration = 60) where T : class
+        {
+            if(key == null)
+                key = getter.GetHashCode().ToString();
+
+            T cachedValue = cache[key] as T;
+
+            if (cachedValue == null)
+            {
+                cachedValue = getter();
+                cache.Insert(key, cachedValue, null, DateTime.Now.AddSeconds(duration), TimeSpan.Zero);
+            }
+
+            return cachedValue;
+        }
+    }
+}
