dba9a36c5874bd848e1870b36f11453f903f671a
Andy Booth <andy.booth.uk@googlemail.com>
2012-01-11 10:49:08 +0000

    Improved Twitter response parsing

--- Instatus/Areas/Twitter/Twitter.cs
+++ Instatus/Areas/Twitter/Twitter.cs
@@ -5,11 +5,16 @@ using System.Web;
 using Instatus.Web;
 using System.Net;
 using System.Text;
+using System.Globalization;
 
 namespace Instatus.Areas.Twitter
 {
     public static class Twitter
     {
+        // http://blog.tacticalnuclearstrike.com/2009/05/twitter-date-parsing-in-c/
+        private const string SearchDateFormat = "ddd, dd MMM yyyy HH:mm:ss zzzz"; // Thu, 06 Oct 2011 19:41:12 +0000
+        private const string StatusesDateFormat = "ddd MMM dd HH:mm:ss zzzz yyyy"; // Mon Jun 27 19:32:19 +0000 2011
+        
         public static string Permalink(string user, string id)
         {
             return string.Format("http://twitter.com/{0}/status/{1}", user, id);
@@ -20,6 +25,7 @@ namespace Instatus.Areas.Twitter
             var uriTemplate = "http://search.twitter.com/search.json?q={0}&include_entities={1}";
             var uri = string.Format(uriTemplate, HttpUtility.UrlEncode(string.Join(" OR ", terms)), includeEntities);
             var response = new WebClient().DownloadJson(uri);
+
             var feed = new List<WebEntry>();
 
             foreach (var entry in response.results)
@@ -29,10 +35,35 @@ namespace Instatus.Areas.Twitter
                     Kind = "Tweet",
                     Source = "Twitter",
                     Description = includeEntities ? ReplaceEntitiesWithHtml(entry.text, entry.entities) : entry.text,
-                    Timestamp = DateTime.Parse(entry.created_at),
+                    Timestamp = DateTime.ParseExact(entry.created_at, SearchDateFormat, CultureInfo.InvariantCulture),
                     Picture = entry.profile_image_url,
-                    User = entry.user,
+                    User = entry.from_user,
                     Uri = Twitter.Permalink(entry.from_user, entry.id_str)
+                });
+            }
+
+            return feed;
+        }
+
+        public static List<WebEntry> Statuses(string screenName, bool includeEntities = true)
+        {
+            var uriTemplate = "https://api.twitter.com/1/statuses/user_timeline.json?screen_name={0}&include_entities={1}";
+            var uri = string.Format(uriTemplate, screenName, includeEntities);
+            var response = new WebClient().DownloadJson(uri);
+
+            var feed = new List<WebEntry>();
+
+            foreach (var entry in response)
+            {
+                feed.Add(new WebEntry()
+                {
+                    Kind = "Tweet",
+                    Source = "Twitter",
+                    Description = includeEntities ? ReplaceEntitiesWithHtml(entry.text, entry.entities) : entry.text,
+                    Timestamp = DateTime.ParseExact(entry.created_at, StatusesDateFormat, CultureInfo.InvariantCulture),
+                    Picture = entry.user.profile_image_url_https,
+                    User = entry.user.screen_name,
+                    Uri = Twitter.Permalink(entry.user.screen_name, entry.id_str)
                 });
             }
 
