1286bdfa2e4a289757691273b317abcb5ebf6728
Andy Booth <andy.booth.uk@googlemail.com>
2012-01-19 22:35:04 +0000

    Instatus.js ajax behaviours eg. $('body').on('submit', 'form', data, instatus.ajax);

---
+++ Instatus/Scripts/Instatus.js
@@ -0,0 +1,42 @@
+﻿window.instatus = {
+    visible: function (el, condition) {
+        if (condition) {
+            el.removeAttr('hidden').show();
+        } else {
+            el.attr('hidden', 'hidden').hide();
+        }
+    },
+    ajax: function (event) {
+        event.preventDefault();
+
+        var config = $.extend({
+            target: 'body',
+            empty: true
+        }, event.data);
+
+        var el = $(this);
+        var target = $(config.target);
+        var container = config.container ? $(config.container) : target;
+
+        var insert = function (html) {
+            if (config.empty)
+                container.empty();
+
+            container.append(html);
+            instatus.visible(target, true);
+        };
+
+        var deferred;
+
+        if (el.is('form')) {
+            deferred = $.post(el.attr('action'), el.serialize())
+                .done(insert);
+        } else {
+            deferred = $.get(el.attr('href'))
+                .done(insert);
+        }
+
+        if (config.addCallbacks)
+            config.addCallbacks(deferred);
+    }
+};
