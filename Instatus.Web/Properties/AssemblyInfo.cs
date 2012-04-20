6327a19701c69cd66e6a3725c9b303295ba462ac
Andy Booth <andy.booth.uk@googlemail.com>
2012-04-20 08:46:26 +0100

    NuGet package

---
+++ Instatus.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Instatus.Web")]
+[assembly: AssemblyDescription("Instatus.Web")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Instatus")]
+[assembly: AssemblyProduct("Instatus.Web")]
+[assembly: AssemblyCopyright("Copyright")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5b99a156-6c16-4042-88b0-7e12b1063333")]
---
+++ Instatus.Web/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(Instatus.Web.Startup), "PreApplicationStart")]
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Instatus.Web.Startup), "PostApplicationStart")]
+
+namespace Instatus.Web
+{
+    public static class Startup
+    {
+        public static Action<ContainerBuilder> Build { get; set; }
+        
+        private static void PreApplicationStart()
+        {
+            
+        }
+
+        private static void PostApplicationStart()
+        {
+            if (Build != null)
+            {
+                var builder = new ContainerBuilder();
+
+                Build(builder);
+
+                DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
+            }
+        }
+    }
+}
