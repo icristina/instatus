61351530e208a50fb3c52111a58a7ed37d200f63
Andy Booth <andy.booth.uk@googlemail.com>
2012-05-01 15:40:18 +0100

    Ensure part collections never null

--- Instatus/Models/Part.cs
+++ Instatus/Models/Part.cs
@@ -15,16 +15,92 @@ namespace Instatus.Models
         public Zone Zone { get; set; }
         public string Scope { get; set; }
         public object RouteData { get; set; }
-        public IList<Element> Elements { get; set; }
-        public IList<Parameter> Parameters { get; set; }
-        public IList<Part> Parts { get; set; }
-        public IList<Link> Links { get; set; }
-        public Query Query { get; set; }
-        public Formatting Formatting { get; set; }
 
-        public Part()
+        private List<Part> parts;
+
+        public List<Part> Parts
         {
-            Formatting = new Formatting();
+            get
+            {
+                if (parts == null)
+                    parts = new List<Part>();
+
+                return parts;
+            }
+            set
+            {
+                parts = value;
+            }
+        }
+
+        private List<Parameter> parameters;
+
+        public List<Parameter> Parameters
+        {
+            get
+            {
+                if (parameters == null)
+                    parameters = new List<Parameter>();
+
+                return parameters;
+            }
+            set
+            {
+                parameters = value;
+            }
+        }
+
+        private List<Link> links;
+
+        public List<Link> Links
+        {
+            get
+            {
+                if (links == null)
+                    links = new List<Link>();
+
+                return links;
+            }
+            set
+            {
+                links = value;
+            }
+        }
+
+        private List<Element> elements;
+
+        public List<Element> Elements
+        {
+            get
+            {
+                if (elements == null)
+                    elements = new List<Element>();
+
+                return elements;
+            }
+            set
+            {
+                elements = value;
+            }
+        }        
+        
+        public Query Query { get; set; } // allow query to be null
+
+        private Formatting formatting;
+
+        public Formatting Formatting
+        {
+            get
+            {
+                if (formatting == null)
+                    formatting = new Formatting();
+
+                return formatting;
+            }
+            set
+            {
+                formatting = value;
+            }
         }
     }
 }
