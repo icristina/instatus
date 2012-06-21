7cfbbcf87dc835d88e6402bf3f5f888393e21bf3
Andy Booth <andy.booth.uk@googlemail.com>
2012-06-21 14:22:38 +0100

    InMemory samples

---
+++ Net45/Instatus/Instatus.Core/InMemory/InMemoryMessageBus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.Core.InMemory
+{
+    public class InMemoryMessageBus : IMessageBus
+    {
+        private IQueue<object> queue = new InMemoryQueue<object>();
+        private IList<object> actions = new List<object>();
+
+        public void Subscribe<T>(Action<T> action)
+        {
+            actions.Add(action);
+        }
+
+        public void Publish<T>(T message)
+        {
+            RunActionsSynchronously(message);
+        }
+
+        private void RunActionsSynchronously<T>(T message)
+        {
+            foreach (var action in actions.OfType<Action<T>>())
+            {
+                action.Invoke(message);
+            }
+        }
+    }
+}
---
+++ Net45/Instatus/Instatus.Core/InMemory/InMemoryQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.Core.InMemory
+{
+    public class InMemoryQueue<T> : IQueue<T>
+    {
+        private ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+        
+        public void Enqueue(T message)
+        {
+            queue.Enqueue(message);
+        }
+
+        public bool TryDequeue(out T message)
+        {
+            return queue.TryDequeue(out message);
+        }
+    }
+}
