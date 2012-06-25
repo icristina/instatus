e4257bf172430f01ebbec80499b91b9a15854694
Andy Booth <andy.booth.uk@googlemail.com>
2012-06-25 10:15:37 +0100

    EF entity storage

---
+++ Net45/Instatus/Instatus.Integration.EntityFramework/EfEntitySet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Instatus.Core;
+
+namespace Instatus.Integration.EntityFramework
+{
+    public class EfEntitySet<T> : IEntitySet<T>
+    {
+        private IDbSet<T> dbset;
+        
+        public T Find(object key)
+        {
+            return dbset.Find(key);
+        }
+
+        public void Delete(object key)
+        {
+            var instance = dbset.Find(key);
+
+            dbset.Remove(instance);
+        }
+
+        public T Create()
+        {
+            var instance = Activator.CreateInstance<T>();
+
+            dbset.Add(instance);
+
+            return instance;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return dbset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return dbset.GetEnumerator();
+        }
+
+        public Type ElementType
+        {
+            get {
+                return dbset.ElementType;
+            }
+        }
+
+        public Expression Expression
+        {
+            get {
+                return dbset.Expression;
+            }
+        }
+
+        public IQueryProvider Provider
+        {
+            get {
+                return dbset.Provider; 
+            }
+        }
+
+        public EfEntitySet(IDbSet<T> dbset)
+        {
+            this.dbset = dbset;
+        }
+    }
+}
---
+++ Net45/Instatus/Instatus.Integration.EntityFramework/EfEntityStorage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Instatus.Core;
+
+namespace Instatus.Integration.EntityFramework
+{
+    public class EfEntityStorage<TContext> : IEntityStorage where TContext : DbContext
+    {
+        private DbContext dbcontext;
+
+        public DbContext Context
+        {
+            get
+            {
+                if (dbcontext == null)
+                    dbcontext = Activator.CreateInstance<TContext>();
+
+                return dbcontext;
+            }
+        }
+        
+        public IEntitySet<T> Set<T>() where T : class
+        {
+            return new EfEntitySet<T>(Context.Set<T>());
+        }
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
