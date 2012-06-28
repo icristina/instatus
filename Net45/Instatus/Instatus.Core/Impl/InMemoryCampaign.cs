ff09d618748dab0b5282038495e2e80afc1a9ef3
Andy Booth <andy.booth.uk@googlemail.com>
2012-06-28 13:13:56 +0100

    InMemory additions

---
+++ Net45/Instatus/Instatus.Core/Impl/InMemoryCampaign.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core.Impl
+{
+    public class InMemoryCampaign : ICampaign
+    {
+        private ISchedule schedule;
+        
+        public ISchedule GetSchedule()
+        {
+            return schedule;
+        }
+
+        public void Subscribe(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public InMemoryCampaign(ISchedule schedule) 
+        {
+            this.schedule = schedule;
+        }
+    }
+
+    public class InMemorySchedule : ISchedule
+    {
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+
+        public InMemorySchedule(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    }
+}
---
+++ Net45/Instatus/Instatus.Core/Impl/InMemoryLocalization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core.Impl
+{
+    public class InMemoryLocalization : ILocalization
+    {
+        private IDictionary<string, string> phrases;
+        
+        public string Phrase(string key)
+        {
+            return phrases[key];
+        }
+
+        public string Format(string key, params object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public InMemoryLocalization(IDictionary<string, string> phrases)
+        {
+            this.phrases = phrases;
+        }
+    }
+}
