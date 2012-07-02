2248ce36eb23ca21898aa84efc53d0108fcf13da
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-02 22:54:56 +0100

    Scaffold web api controller

---
+++ Net45/Instatus/Instatus.Core/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core
+{
+    public interface IMapper
+    {
+        T Map<T>(object source);
+        void Inject(object target, object source);
+    }
+}
---
+++ Net45/Instatus/Instatus.Integration.WebApi/EntityStorageApiController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using System.Net.Http;
+using Instatus.Core;
+using System.Net;
+
+namespace Instatus.Integration.WebApi
+{
+    public class EntityStorageApiController<TEntity, TModel> : ApiController where TEntity : class
+    {
+        private IEntityStorage entityStorage;
+        private IMapper mapper;
+
+        [Queryable]
+        public IQueryable<TEntity> Get()
+        {
+            return entityStorage.Set<TEntity>();
+        }
+
+        public TModel Get(int id)
+        {
+            var entity = entityStorage.Set<TEntity>().Find(id);
+            
+            if (entity == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            var model = mapper.Map<TModel>(entity);
+
+            return model;
+        }
+
+        public HttpResponseMessage Put(int id, TModel model)
+        {
+            if (ModelState.IsValid && id == (model as dynamic).Id)
+            {
+                var entity = entityStorage.Set<TEntity>().Find(id);
+
+                mapper.Inject(entity, model);
+
+                try
+                {
+                    entityStorage.SaveChanges();
+                }
+                catch
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, model);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        public HttpResponseMessage Post(TModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = mapper.Map<TEntity>(model);
+                
+                entityStorage.Set<TEntity>().Add(entity);
+                entityStorage.SaveChanges();
+
+                var response = Request.CreateResponse(HttpStatusCode.Created, model);
+                
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = (model as dynamic).Id }));
+                
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            var entity = entityStorage.Set<TEntity>().Find(id);
+            
+            if (entity == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            entityStorage.Set<TEntity>().Delete(id);
+
+            try
+            {
+                entityStorage.SaveChanges();
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            var model = mapper.Map<TModel>(entity);
+
+            return Request.CreateResponse(HttpStatusCode.OK, model);
+        }
+
+        public EntityStorageApiController(IEntityStorage entityStorage, IMapper mapper)
+        {
+            this.entityStorage = entityStorage;
+            this.mapper = mapper;
+        }
+    }
+}
