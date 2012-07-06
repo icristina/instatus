0ac96f9869d955c55ad7a4ff727ba9eba1982851
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-06 17:55:18 +0100

    Linguistics service

---
+++ Net45/Instatus/Instatus.Core/ILinguistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core
+{
+    public interface ILinguistics
+    {
+        string Pluralize(string text);
+        string Singularize(string text);
+        string Suggestions(string text);
+    }
+}
---
+++ Net45/Instatus/Instatus.Integration.Server/DefaultLinguistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Design.PluralizationServices;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Instatus.Core;
+
+namespace Instatus.Integration.Server
+{
+    public class DefaultLinguistics : ILinguistics
+    {
+        private PluralizationService pluralizationService = PluralizationService.CreateService(new CultureInfo("en-GB"));
+
+        public string Pluralize(string text)
+        {
+            return pluralizationService.Pluralize(text);
+        }
+
+        public string Singularize(string text)
+        {
+            return pluralizationService.Singularize(text);
+        }
+
+        public string Suggestions(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
