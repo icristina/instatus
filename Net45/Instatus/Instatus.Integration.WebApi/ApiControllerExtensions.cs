4ad9d7547b495ade2f024cdf3a9ba053c96c72c4
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-10 16:17:10 +0100

    Enable unit testing of webapi controller

---
+++ Net45/Instatus/Instatus.Integration.WebApi/ApiControllerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace Instatus.Integration.WebApi
+{
+    public static class ApiControllerExtensions
+    {
+        public static T AllowTesting<T>(this T apiController) where T : ApiController
+        {
+            apiController.Request = new HttpRequestMessage();
+            apiController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+            return apiController;
+        }
+    }
+}
