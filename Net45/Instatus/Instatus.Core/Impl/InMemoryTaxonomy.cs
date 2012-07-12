f08e90258f67d589f1bb93e18e2dfc4abb725cc9
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-12 18:34:25 +0100

    Taxonomy

---
+++ Net45/Instatus/Instatus.Core/Impl/InMemoryTaxonomy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core.Impl
+{
+    public class InMemoryTaxonomy : ITaxonomy
+    {
+        private string[] tags;
+        
+        public string[] GetTags()
+        {
+            return tags;
+        }
+
+        public InMemoryTaxonomy(string[] tags)
+        {
+            this.tags = tags;
+        }
+    }
+}
