b10786481f4ea1f0021ebdf712bab9f95ace9975
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-16 08:10:30 +0100

    RazorGenerator based scaffold views

---
+++ Net45/Instatus/Instatus.Scaffold/App_Start/RazorGeneratorMvcStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGenerator.Mvc;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Instatus.Scaffold.App_Start.RazorGeneratorMvcStart), "Start")]
+
+namespace Instatus.Scaffold.App_Start {
+    public static class RazorGeneratorMvcStart {
+        public static void Start() {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+            };
+
+            ViewEngines.Engines.Insert(0, engine);
+
+            // StartPage lookups are done by WebPages. 
+            VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
+        }
+    }
+}
