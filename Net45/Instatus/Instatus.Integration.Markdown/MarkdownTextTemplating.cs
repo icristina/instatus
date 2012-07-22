09b710d9dbd23714fe0048e81297462a99f077d1
Andy Booth <andy.booth.uk@googlemail.com>
2012-07-22 20:44:29 +0100

    TextTemplating

---
+++ Net45/Instatus/Instatus.Integration.Markdown/MarkdownTextTemplating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Instatus.Core;
+
+namespace Instatus.Integration.Markdown
+{
+    public class MarkdownTextTemplating : ITextTemplating
+    {
+        public void Render(string viewName, object viewData, Stream outputStream)
+        {
+            var content = viewData.ToString();
+            var markdown = new MarkdownSharp.Markdown();
+            var output = markdown.Transform(content);
+
+            using (var streamWriter = new StreamWriter(outputStream))
+            {
+                streamWriter.Write(output);
+            }
+        }
+    }
+}
