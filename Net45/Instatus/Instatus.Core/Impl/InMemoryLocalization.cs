45dd3ed81b10c1fb31eb3402c60853ecde9899e7
Andy Booth <andy.booth.uk@googlemail.com>
2012-09-12 10:28:20 +0100

    First prototype of Localization and Session implementations

--- Net45/Instatus/Instatus.Core/Impl/InMemoryLocalization.cs
+++ Net45/Instatus/Instatus.Core/Impl/InMemoryLocalization.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,21 +8,40 @@ namespace Instatus.Core.Impl
 {
     public class InMemoryLocalization : ILocalization
     {
-        private IDictionary<string, string> phrases;
+        private ISession session;
         
+        // Tuple<locale, phraseKey>
+        private static IDictionary<Tuple<string, string>, string> phrases = new ConcurrentDictionary<Tuple<string, string>, string>();
+        public const string DefaultLocale = "en_US";
+
         public string Phrase(string key)
         {
-            return phrases[key];
+            var localeKey = new Tuple<string, string>(session.Locale, key);
+            var defaultLocaleKey = new Tuple<string, string>(DefaultLocale, key);
+            
+            return phrases[localeKey] ?? phrases[defaultLocaleKey];
         }
 
         public string Format(string key, params object[] values)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return string.Format(Phrase(key), values);
+            }
+            catch
+            {
+                return string.Join(", ", values);
+            }
         }
 
-        public InMemoryLocalization(IDictionary<string, string> phrases)
+        public InMemoryLocalization(ISession session)
         {
-            this.phrases = phrases;
+            this.session = session;
+        }
+
+        public static void AddOrUpdate(IDictionary<Tuple<string, string>, string> newPhrases) 
+        {
+            newPhrases.ToList().ForEach(x => phrases.Add(x.Key, x.Value));
         }
     }
 }
---
+++ Net45/Instatus/Instatus.Integration.Server/AspNetSession.cs
@@ -0,0 +1,85 @@
+﻿using Instatus.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading;
+
+namespace Instatus.Integration.Server
+{
+    public class AspNetSession : ISession
+    {
+        public string SiteName
+        {
+            get 
+            {
+                return HttpContext.Current.Request.Url.Host;
+            }
+        }
+
+        private const string cookieKey = "userPreferences";
+        private const string localeKey = "locale";
+
+        private string locale;
+
+        // override, for example if sourced from facebook signed_request, this takes highest priority
+        public virtual string GetCustomLocale() 
+        {
+            return null;
+        }
+
+        // format en_GB, en_US
+        public string Locale
+        {
+            get
+            {
+                // order of precedence
+                // [1] already parsed in memory, requires AspNetSession to be declared as request scope
+                // [2] query string
+                // [3] cookie
+                // [4] thread culture
+                if (locale == null)
+                {
+                    var request = HttpContext.Current.Request;
+                    var localeCustomValue = GetCustomLocale();
+                    var localeRequestValue = request.Params[localeKey];
+                    var localeCookieValue = request.Cookies[cookieKey][localeKey];
+
+                    CultureInfo culture = Thread.CurrentThread.CurrentCulture;
+
+                    try
+                    {
+                        culture = new CultureInfo(GetCustomLocale() ?? localeRequestValue ?? localeCookieValue);
+                        Thread.CurrentThread.CurrentCulture = culture;
+                    }
+                    catch
+                    {
+                        // use default culture
+                    }
+
+                    locale = culture.Name;
+
+                    PersistLocale();
+                }
+
+                return locale;
+            }
+            set
+            {
+                if (!locale.Equals(value))
+                {
+                    locale = value;
+                    PersistLocale();
+                }
+            }
+        }
+
+        private void PersistLocale()
+        {
+            var response = HttpContext.Current.Response;
+            response.Cookies[cookieKey][localeKey] = locale;
+        }
+    }
+}
