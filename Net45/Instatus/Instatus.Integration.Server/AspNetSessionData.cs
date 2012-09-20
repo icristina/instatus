c2ddf7ae97639c5f12633172411c1d6cf225d08f
Andy Booth <andy.booth.uk@googlemail.com>
2012-09-20 17:39:51 +0100

    AspNetSession uses Accept-Language to process locale

--- Net45/Instatus/Instatus.Integration.Server/AspNetSessionData.cs
+++ Net45/Instatus/Instatus.Integration.Server/AspNetSessionData.cs
@@ -11,9 +11,9 @@ namespace Instatus.Integration.Server
 {
     public class AspNetSessionData : ISessionData
     {
-        public string SiteName
+        public virtual string SiteName
         {
-            get 
+            get
             {
                 return HttpContext.Current.Request.Url.Host;
             }
@@ -25,9 +25,15 @@ namespace Instatus.Integration.Server
         private string locale;
 
         // override, for example if sourced from facebook signed_request, this takes highest priority
-        public virtual string GetLocale() 
+        public virtual string GetCustomLocale()
         {
             return null;
+        }
+
+        // eg. Accept-Language:en-GB,en-US;q=0.8,en;q=0.6
+        public string ParseAcceptLanguage(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value) ? value.Split(',', ';').FirstOrDefault() : string.Empty;
         }
 
         // format en-GB, en-US, de-DE
@@ -39,14 +45,16 @@ namespace Instatus.Integration.Server
                 // [1] already parsed in memory, requires AspNetSession to be declared as request scope
                 // [2] query string or form param
                 // [3] cookie
-                // [4] thread culture
+                // [4] accept language from browser
+                // [5] thread culture
                 if (locale == null)
                 {
                     var request = HttpContext.Current.Request;
-                    
-                    this.Locale = GetLocale() ??
+
+                    this.Locale = GetCustomLocale() ??
                         request.Params[localeKey] ??
-                        (request.Cookies[cookieKey] != null ? request.Cookies[cookieKey][localeKey] : null);
+                        (request.Cookies[cookieKey] != null ? request.Cookies[cookieKey][localeKey] : null) ??
+                        ParseAcceptLanguage(request.Headers["Accept-Language"]);
                 }
 
                 return locale;
@@ -64,7 +72,7 @@ namespace Instatus.Integration.Server
                     culture = Thread.CurrentThread.CurrentCulture;
                 }
 
-                HttpContext.Current.Response.Cookies[cookieKey][localeKey] = locale = culture.Name;                
+                HttpContext.Current.Response.Cookies[cookieKey][localeKey] = locale = culture.Name;
             }
         }
     }
