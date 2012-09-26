0fdab7f3dc2470a4eba7831f751fe09e44d0f3af
Andy Booth <andy.booth.uk@googlemail.com>
2012-09-26 17:00:29 +0100

    Base.css and Base.js

---
+++ Net45/Instatus/Instatus.Scaffold/Scripts/base.js
@@ -0,0 +1,20 @@
+﻿function chain() {
+    var a = arguments;
+    return function () {
+        for (var i in a) {
+            if ($.isFunction(a[i])) a[i]();
+        }
+    };
+}
+
+function contains(array, item) {
+    return array.indexOf(item) != -1;
+}
+
+function remove(array, item) {
+    var index = array.indexOf(item);
+
+    if (index != -1) {
+        array.splice(index, 1);
+    }
+}
