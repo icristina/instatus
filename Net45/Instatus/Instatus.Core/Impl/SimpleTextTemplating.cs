1928bebd30839ea04b21fccfa287adfa37dbe8da
Andy Booth <andy.booth.uk@googlemail.com>
2012-09-27 17:53:16 +0100

    AngularJs integration

---
+++ Net45/Instatus/Instatus.Core/Impl/SimpleTextTemplating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Instatus.Core.Impl
+{
+    public class SimpleTextTemplating : ITextTemplating
+    {
+        public void Render(string viewName, object viewData, Stream outputStream)
+        {
+            using (var streamWriter = new StreamWriter(outputStream))
+            {
+                streamWriter.Write(viewData.ToString());
+            }
+        }
+    }
+}
