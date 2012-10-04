a6e4425ee05212c1c80da1a813cacb42a9783b0d
Andy Booth <andy.booth.uk@googlemail.com>
2012-10-04 17:26:47 +0100

    Browser capabilities html helper

---
+++ Net45/Instatus/Instatus.Integration.Mvc/HtmlHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Instatus.Integration.Mvc
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString BrowserCapabilitiesHint<T>(this HtmlHelper<T> htmlHelper)
+        {
+            var browserCapabilities = htmlHelper.ViewContext.RequestContext.HttpContext.Request.Browser;
+            var sb = new StringBuilder();
+
+            sb.Append("no-js ");
+            sb.Append(browserCapabilities.Browser.ToLower());
+            sb.Append(" ");
+            sb.Append(browserCapabilities.Browser.ToLower());
+            sb.Append(browserCapabilities.MajorVersion);
+
+            return new MvcHtmlString(sb.ToString());
+        }
+    }
+}
