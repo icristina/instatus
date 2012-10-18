1743d93372a3f6de9ff72e4a5b3d765a1d623e55
Andy Booth <andy.booth.uk@googlemail.com>
2012-10-18 12:50:53 +0100

    Helpers for simple FormsAuthentication

---
+++ Instatus.Integration.Server/FormsAuthenticationMembership.cs
@@ -0,0 +1,52 @@
+﻿using Instatus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace Instatus.Integration.Server
+{
+    // allow users to be specified in Web.Config
+    //<authentication mode="Forms">
+    //  <forms loginUrl="~/Account/Login">
+    //    <credentials passwordFormat="Clear">
+    //      <user name="emailAddress" password="password" />
+    //    </credentials>
+    //  </forms>
+    //</authentication>
+    public class FormsAuthenticationMembership : IMembership
+    {
+        public bool ValidateUser(string userName, string password)
+        {
+            return FormsAuthentication.Authenticate(userName, password);
+        }
+
+        public bool ValidateExternalUser(string providerName, string providerUserId, IDictionary<string, string> data, out string userName)
+        {
+            userName = string.Empty;
+            return false;
+        }
+
+        public string[] GetRoles(string userName)
+        {
+            return new string[] { };
+        }
+
+        public string GenerateVerificationToken(string userName)
+        {
+            return null;
+        }
+
+        public bool ValidateVerificationToken(string userName, string verificationToken)
+        {
+            return false;
+        }
+
+        public void ChangePassword(string userName, string verificationToken, string newPassword)
+        {
+
+        }
+    }
+}
