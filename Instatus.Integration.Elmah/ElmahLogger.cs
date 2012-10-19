df97ac77be4d512f0a4b83e8e2b9673217c575fc
Andy Booth <andy.booth.uk@googlemail.com>
2012-10-19 15:52:22 +0100

    Simpler Elmah logging

---
+++ Instatus.Integration.Elmah/ElmahLogger.cs
@@ -0,0 +1,18 @@
+﻿using Elmah;
+using Instatus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.Integration.Elmah
+{
+    public class ElmahLogger : ILogger
+    {
+        public void Log(Exception exception, IDictionary<string, string> properties)
+        {
+            ErrorSignal.FromCurrentContext().Raise(exception);
+        }
+    }
+}
