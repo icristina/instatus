739284e733b3e2e9568ce3c3dfc5f108c677699c
Andy Booth <andy.booth.uk@googlemail.com>
2012-11-23 09:26:39 +0000

    Custom IsInRole for enum

---
+++ Instatus.Scaffold/PrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Instatus.Scaffold.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Instatus.Scaffold
+{
+    public static class PrincipalExtensions
+    {
+        public static bool IsInRole(this IPrincipal principal, Role role)
+        {
+            return principal.IsInRole(role.ToString());
+        }
+    }
+}
