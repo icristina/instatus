861f70f274e72c63b9cff8cfa54cb811ed7afca9
Andy Booth <andy.booth.uk@googlemail.com>
2012-12-20 22:14:34 +0000

    Phase enum; payload extension methods

---
+++ Instatus.Scaffold/Entities/PayloadExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Instatus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Instatus.Scaffold.Entities
+{
+    public static class PayloadExtensions
+    {
+        public static void SetPayload<T>(this IPayload entity, T obj, IJsonSerializer jsonSerializer = null)
+        {
+            entity.Data = (jsonSerializer ?? DependencyResolver.Current.GetService<IJsonSerializer>()).Stringify(obj);
+        }
+
+        public static T GetPayload<T>(this IPayload entity, IJsonSerializer jsonSerializer = null)
+        {
+            return (jsonSerializer ?? DependencyResolver.Current.GetService<IJsonSerializer>()).Parse<T>(entity.Data);
+        }
+    }
+}
---
+++ Instatus.Scaffold/Entities/Phase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Instatus.Scaffold.Entities
+{
+    public enum Phase
+    {
+        Publish,
+        Open,
+        Close,
+        Archive
+    }
+}
