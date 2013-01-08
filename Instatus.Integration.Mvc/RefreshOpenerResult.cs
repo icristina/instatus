d713ad2edd95d6db1b0031b004b9cd948ce385a5
Andy Booth <andy.booth.uk@googlemail.com>
2013-01-08 13:03:04 +0000

    ActionResult to close current tab and refresh opener; for admin views

---
+++ Instatus.Integration.Mvc/RefreshOpenerResult.cs
@@ -0,0 +1,21 @@
+﻿using Instatus.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Instatus.Integration.Mvc
+{
+    public class RefreshOpenerResult : ActionResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.RequestContext.HttpContext.Response;
+
+            response.ContentType = WellKnown.ContentType.Html;
+            response.Write("<script>window.opener.location.reload(); window.close();</script>");
+        }
+    }
+}
