2847bf95ea0320cc64191f4cbcd226f76b23744c
Andy Booth <andy.booth.uk@googlemail.com>
2013-01-14 13:51:21 +0000

    FileHelper datetime format string

---
+++ Instatus.Integration.FileHelper/ConverterConfig.cs
@@ -0,0 +1,14 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.Integration.FileHelper
+{
+    public class ConverterConfig
+    {
+        public const string DateTimeFormatString = "yyyy-MM-dd HH:mm:ss";
+    }
+}
