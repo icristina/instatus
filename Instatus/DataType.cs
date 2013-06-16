6a62b8e8a2b5f5b610c48de42beb4f8ce9871489
Andy Booth <andy.booth.uk@googlemail.com>
2013-06-16 13:43:06 +0100

    FieldViewModel

---
+++ Instatus/DataType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus
+{
+    public enum DataType
+    {
+        // as System.ComponentModel.DataAnnotations.DataType        
+        Date,
+        DateTime,
+        EmailAddress,
+        MultilineText,
+        PhoneNumber,
+        PostalCode,
+        Text,
+        // custom
+        Number,
+        Boolean
+    }
+}
---
+++ Instatus/ViewModels/FieldViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.ViewModels
+{
+    public class FieldViewModel : BindableBase
+    {
+        private string title;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                SetProperty(ref title, value);
+            }
+        }
+
+        private object value;
+
+        public object Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                SetProperty(ref this.value, value);
+            }
+        }
+
+        private DataType dataType = DataType.Text;
+
+        public DataType DataType
+        {
+            get
+            {
+                return dataType;
+            }
+            set
+            {
+                SetProperty(ref dataType, value);
+            }
+        }
+
+        private bool required;
+
+        public bool Required
+        {
+            get
+            {
+                return required;
+            }
+            set
+            {
+                SetProperty(ref required, value);
+            }
+        }
+
+        private string pattern;
+
+        public string Pattern
+        {
+            get
+            {
+                return pattern;
+            }
+            set
+            {
+                SetProperty(ref pattern, value);
+            }
+        }
+
+        private int minLength = 0;
+
+        public int MinLength
+        {
+            get
+            {
+                return minLength;
+            }
+            set
+            {
+                SetProperty(ref minLength, value);
+            }
+        }
+
+        private int maxLength = int.MaxValue;
+
+        public int MaxLength
+        {
+            get
+            {
+                return maxLength;
+            }
+            set
+            {
+                SetProperty(ref maxLength, value);
+            }
+        }
+
+        private int min = 0;
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                SetProperty(ref min, value);
+            }
+        }
+
+        private int max = int.MaxValue;
+
+        public int Max
+        {
+            get
+            {
+                return max;
+            }
+            set
+            {
+                SetProperty(ref max, value);
+            }
+        }
+    }
+}
