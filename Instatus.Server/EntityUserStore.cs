7a2e5b53f25c9c1f4e8c9135eb2914890e4bcd97
Andy Booth <andy.booth.uk@googlemail.com>
2013-10-25 06:33:21 +0100

    Entity user store

---
+++ Instatus.Server/EntityUserStore.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instatus.Server
+{
+    public class EntityUserStore<TContext, TUser> : IUserStore<TUser>
+        where TContext : DbContext, new()
+        where TUser : class, IUser
+    {
+        public Task CreateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TUser> FindByNameAsync(string userName)
+        {
+            using (var context = new TContext())
+            {
+                return await context.Set<TUser>().FirstOrDefaultAsync(u => u.UserName == userName);
+            }
+        }
+
+        public Task UpdateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
